05d85a36d29dfb43e97593020cd87c78c18453ee
Samuel Painter <sam.foy.painter@gmail.com>
2022-12-03 22:17:53 -0800

    day4

--- src/io/github/samfoy/aoc2022/Utils.kt
+++ src/io/github/samfoy/aoc2022/Utils.kt
@@ -14,3 +14,8 @@ fun readInput(name: String) = File("src", name)
 //fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
 //    .toString(16)
 //    .padStart(32, '0')
+
+fun <T> List<T>.toPair(): Pair<T, T> {
+    if (this.size > 2) throw IllegalArgumentException()
+    return Pair(this.first(), this.last())
+}
---
+++ src/io/github/samfoy/aoc2022/day4/Day04.kt
@@ -0,0 +1,30 @@
+package io.github.samfoy.aoc2022.day4
+
+import io.github.samfoy.aoc2022.readInput
+import io.github.samfoy.aoc2022.toPair
+
+typealias Assignment = List<Int>
+typealias AssignmentPair = Pair<Assignment, Assignment>
+
+fun main() {
+
+    fun part1(input: List<AssignmentPair>) = input
+        .map { it.first.containsAll(it.second) || it.second.containsAll(it.first) }
+        .count { it }
+
+    fun part2(input: List<AssignmentPair>) = input
+        .map { it.first.intersect(it.second.toSet()) }
+        .count { it.isNotEmpty() }
+
+    val input = readInput("resources/Day04").lines()
+        .filter { it.isNotBlank() }
+        .map { pair ->
+            pair.split(",")
+                .map { sub -> sub.split("-").map(String::toInt) }
+                .map { (it.first()..it.last()).toList() }
+                .toPair()
+        }
+
+    check(part1(input) == 599)
+    check(part2(input) == 928)
+}
